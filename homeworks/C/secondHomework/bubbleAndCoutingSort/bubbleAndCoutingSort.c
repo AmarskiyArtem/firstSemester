a121ce338fe5f81e9005562e0bc57dcfb3eeab19
AmarskiyArtem <amarskiywork@gmail.com>
2022-09-22 22:59:55 +0300

    bubbleSort done

--- homeworks/C/secondHomework/bubbleAndCoutingSort/bubbleAndCoutingSort.c
+++ homeworks/C/secondHomework/bubbleAndCoutingSort/bubbleAndCoutingSort.c
@@ -1,1 +1,26 @@
-﻿
+﻿#include<stdio.h>
+#include<locale.h>
+
+void swap(int* a, int* b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
+
+void bubbleSort(int *array, int arraySize) {
+    for (int i = 0; i < arraySize - 1; ++i) {
+        for (int j = arraySize - 1; j > i; --j) {
+            if (array[j - 1] > array[j]) {
+                swap(&array[j - 1], &array[j]);
+            }
+        }
+    }
+}
+
+void countingSort(int* array, int arraySize) {
+
+}
+
+void main() {
+
+}
