012b558e2b0aff4676cba1044e2668b208663635
AmarskiyArtem <amarskiywork@gmail.com>
2022-09-25 02:55:46 +0300

    update

--- homeworks/C/secondHomework/bubbleAndCoutingSort/bubbleAndCoutingSort.c
+++ homeworks/C/secondHomework/bubbleAndCoutingSort/bubbleAndCoutingSort.c
@@ -3,11 +3,14 @@
 #include<malloc.h>
 #include<stdbool.h>
 #include<stdlib.h>
+#include<time.h>
+
+#define arrayLength 30000
 
 void swap(int* a, int* b) {
-    *a = *a + *b;
-    *b = *a - *b;
-    *a = *a - *b;
+    *a = *a ^ *b;
+    *b = *b ^ *a;
+    *a = *a ^ *b;
 }
 
 void bubbleSort(int *array, int arraySize) {
@@ -51,45 +54,78 @@ void countingSort(int* array, int arraySize) {
     free(arrayForCounting);
 }
 
-bool equalityTest(void) {
-    for (int j = 0; j < 5; ++j) {
-        int arraysSize = 5 + rand() % 30;
-        int* arrayOne = (int*)calloc(arraysSize, sizeof(int));
-        if (arrayOne == NULL)
-        {
-            printf("Всё очень плохо :(");
+void arrayPrint(int* array, int arraySize) {
+    for (int i = 0; i < arraySize; ++i) {
+        printf("%d ", array[i]);
+    }
+}
+bool sortChecker(int* array, int arraySize) {
+    for (int i = 0; i < arraySize - 1; ++i) {
+        if (array[i] > array[i + 1]) {
             return false;
         }
-        int* arrayOneCopy = (int*)calloc(arraysSize, sizeof(int));
-        if (arrayOneCopy == NULL)
-        {
-            printf("Всё очень плохо :(");
-            return false;
-        }
-        for (int i = 0; i < arraysSize; ++i) {
-            arrayOne[i] = -100 + rand() % 200;
-        }
-        for (int i = 0; i < arraysSize; ++i) {
-            arrayOneCopy[i] = arrayOne[i];
-        }
-        bubbleSort(&arrayOne, arraysSize);
-        countingSort(&arrayOneCopy, arraysSize);
-        for (int i = 0; i < arraysSize; ++i) {
-            if (arrayOne[i] != arrayOneCopy[i]) {
-                free(arrayOne);
-                free(arrayOneCopy);
-                return false;
-            }
-        }
-        free(arrayOne);
-        free(arrayOneCopy);
+    }
+    return true;
+}
+bool bubbleSortTests(void) {
+    int arrayOne[15] = { 6, 8, 24, 67, 544, 53, 23, 63, 94, 13, 33, 1, 4, -5, 34 };
+    bubbleSort(&arrayOne, 15);
+    if (!sortChecker(&arrayOne, 15)) {
+        return false;
+    }
+    int arrayTwo[20] = { 68, 36, 46, 89, 96, 45, 67, 29, 91, 66, 40, 20, 84, 17, 97, 47, 40, 81, 72, 84 };
+    bubbleSort(&arrayTwo, 20);
+    if (!sortChecker(&arrayTwo, 20)) {
+        return false;
+    }
+    return true;
+}
+
+bool countingSortTests(void) {
+    int arrayOne[15] = { 6, 8, 24, 67, 544, 53, 23, 63, 94, 13, 33, 1, 4, -5, 34 };
+    countingSort(&arrayOne, 15);
+    if (!sortChecker(&arrayOne, 15)) {
+        return false;
+    }
+    int arrayTwo[20] = { 68, 36, 46, 89, 96, 45, 67, 29, 91, 66, 40, 20, 84, 17, 97, 47, 40, 81, 72, 84 };
+    countingSort(&arrayTwo, 20);
+    if (!sortChecker(&arrayTwo, 20)) {
+        return false;
     }
     return true;
 }
 
 
+
 void main() {
     srand(time(NULL));
-    bool x = equalityTest();
-    x ? printf("YES") : printf("NO");
+    setlocale(LC_ALL, "RU");
+    if (!bubbleSortTests()) { 
+        printf("ААААА ПАНИКА НЕ РАБОТАЕТ"); 
+    }
+    if (!countingSortTests()) {
+        printf("ААААА ПАНИКА НЕ РАБОТАЕТ(2)");
+    }
+    
+    int arrayForBubbleSort[arrayLength] = { 0 };
+    for (int i = 0; i < arrayLength; ++i) {
+        arrayForBubbleSort[i] = -1000 + rand() % 2000;
+    }
+    clock_t startBubble;
+    clock_t stopBubble;
+    startBubble = clock();
+    bubbleSort(&arrayForBubbleSort, arrayLength);
+    stopBubble = clock();
+    printf("Bubble sort required %f seconds\n", (stopBubble - startBubble) / CLK_TCK);
+    
+    int arrayForCountingSort[arrayLength] = { 0 };
+    for (int i = 0; i < arrayLength; ++i) {
+        arrayForCountingSort[i] = -1000 + rand() % 2000;
+    }
+    clock_t startCounting;
+    clock_t stopCounting;
+    startCounting = clock();
+    countingSort(&arrayForCountingSort, arrayLength);
+    stopCounting = clock();
+    printf("Counting sort required %f seconds\n", (stopCounting - startCounting) / CLK_TCK);
 }
---
+++ homeworks/C/secondHomework/halfQsort/halfQsort.c
@@ -0,0 +1,56 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+#include<locale.h>
+#include<stdbool.h>
+
+
+void swap(int* a, int* b) {
+    *a = *a ^ *b;
+    *b = *b ^ *a;
+    *a = *a ^ *b;
+}
+
+void arrayPrint(int* array, int arraySize) {
+    for (int i = 0; i < arraySize; ++i) {
+        printf("%d ", array[i]);
+    }
+}
+
+void halfQsort(int* array, int arraySize) {
+    int left = 0;
+    int right = arraySize - 1;
+    while (left < right) {
+        if (array[left] < array[0]) {
+            ++left;
+        }
+        else if (array[right] >= array[0]) {
+            --right;
+        }
+        else {
+            swap(&array[right], &array[left]);
+            ++left;
+            --right;
+        }
+    }
+}
+
+void main() {
+    setlocale(LC_ALL, "RU");
+    srand(time(NULL));
+    int arraySize = 10 + rand() % 10;
+    printf("Исходный массив:\n");
+    int* array = (int*)calloc(arraySize, sizeof(int));
+    if (array == NULL)
+    {
+        printf("Всё очень плохо :(");
+        return;
+    }
+    for (int i = 0; i < arraySize; ++i) {
+        array[i] = rand() % 100;
+        printf("%d ", array[i]);
+    }
+    halfQsort(&array, arraySize);
+    printf("\nМассив после изменений:\n");
+    arrayPrint(&array, arraySize);
+}
