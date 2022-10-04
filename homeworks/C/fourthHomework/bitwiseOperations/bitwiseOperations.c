13ee60f3549a7210790f075f26e23388f3be71a5
AmarskiyArtem <amarskiywork@gmail.com>
2022-10-05 01:43:44 +0300

    add sum and user interaction

--- homeworks/C/fourthHomework/bitwiseOperations/bitwiseOperations.c
+++ homeworks/C/fourthHomework/bitwiseOperations/bitwiseOperations.c
@@ -1,11 +1,10 @@
 ﻿#include <stdio.h>
 #include <locale.h>
-
-const int sizeOfArrayWithBinaryNumber = sizeof(char) * 8;
+#define sizeOfArrayWithBinaryNumber sizeof(int) * 8
 
 void arrayPrint(int* array, int arraySize) {
     for (int i = 0; i < arraySize; ++i) {
-        printf("%d ", array[i]);
+        printf("%d", array[i]);
     }
 }
 
@@ -26,14 +25,38 @@ int decimalConverter(int* binaryNumber) {
     }
     return number;
 }
+void sumOfTwoBinaryNumbers(int* firstNumber, int* secondNumber, int* sumResult) {
+    int extraUnit = 0;
+    for (int i = sizeOfArrayWithBinaryNumber - 1; i >= 0; --i) {
+        int currentSum = firstNumber[i] + secondNumber[i] + extraUnit;
+        sumResult[i] = (currentSum) % 2;
+        extraUnit = (currentSum) / 2;
+    }
+}
 
 void main() {
     setlocale(LC_ALL, "RU");
-    printf("Введите число:\n");
-    int number = 0;
-    scanf_s("%d", &number);
-    int binaryNumber[8] = { 0 };
-    binaryConverter(&binaryNumber[0], number);
-    printf("Число в двоичном представлении: \n");
-    arrayPrint(&binaryNumber[0], sizeOfArrayWithBinaryNumber);
+    
+    printf("Введите первое число:\n");
+    int firstNumber = 0;
+    scanf_s("%d", &firstNumber);
+    printf("Введите второе число:\n");
+    int secondNumber = 0;
+    scanf_s("%d", &secondNumber);
+
+    int firstBinaryNumber[sizeOfArrayWithBinaryNumber] = { 0 };
+    binaryConverter(&firstBinaryNumber[0], firstNumber);
+    int secondBinaryNumber[sizeOfArrayWithBinaryNumber] = { 0 };
+    binaryConverter(&secondBinaryNumber[0], secondNumber);
+    printf("Первое число в двоичном представлении:\n");
+    arrayPrint(&firstBinaryNumber[0], sizeOfArrayWithBinaryNumber);
+    printf("\nВторое число в двоичном представлении:\n");
+    arrayPrint(&secondBinaryNumber[0], sizeOfArrayWithBinaryNumber);
+
+    int resultOfSumOfBinaryNumbers[sizeOfArrayWithBinaryNumber] = { 0 };
+    sumOfTwoBinaryNumbers(&firstBinaryNumber[0], &secondBinaryNumber[0], &resultOfSumOfBinaryNumbers[0]);
+    printf("\nИх сумма в двоичном представлении:\n");
+    arrayPrint(&resultOfSumOfBinaryNumbers[0], sizeOfArrayWithBinaryNumber);
+    int decimalResult = decimalConverter(&resultOfSumOfBinaryNumbers[0]);
+    printf("\nВ десятичном:\n%d", decimalResult);
 }
