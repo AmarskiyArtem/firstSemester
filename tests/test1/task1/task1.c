70c88b555b9ffc970bebf8576858379d7411de9f
AmarskiyArtem <amarskiywork@gmail.com>
2022-10-05 16:55:21 +0300

    add better realization

--- tests/test1/task1/task1.c
+++ tests/test1/task1/task1.c
@@ -2,6 +2,29 @@
 #include <stdbool.h>
 #include <locale.h>
 
+int betterSumOfHonestFibonacciNumbers(int rightBoard) {
+    int result = 0;
+    if (rightBoard < 3) {
+        return result;
+    }
+    int currentFibonacciNumber = 2;
+    int fibonacciArray[2] = { 1, 1 };
+    for (int i = 0; true; ++i) {
+        if (currentFibonacciNumber > rightBoard) {
+            break;
+        }
+        if (currentFibonacciNumber % 2 == 0) {
+            result += currentFibonacciNumber;
+        }
+
+        fibonacciArray[0] = fibonacciArray[1];
+        fibonacciArray[1] = currentFibonacciNumber;
+        currentFibonacciNumber = fibonacciArray[0] + fibonacciArray[1];
+    }
+    return result;
+}
+
+//первая версия реализации, каждый раз заново вычисляет число, кажется, это не очень хорошо
 int iterativeFibonacci(int nFibonacciNumber) {
     if (nFibonacciNumber < 3) {
         return 1;
@@ -54,10 +77,35 @@ bool tests(void) {
     }
 }
 
+bool testsForBetterFunction(void) {
+    if (betterSumOfHonestFibonacciNumbers(200) != 188) {
+        printf("Первый тест не прошел");
+        return false;
+    }
+
+    if (betterSumOfHonestFibonacciNumbers(15000) != 14328) {
+        printf("Второй тест не прошел");
+        return false;
+    }
+
+    if (betterSumOfHonestFibonacciNumbers(0) != 0) {
+        printf("Третий тест не прошел");
+        return false;
+    }
+
+    if (betterSumOfHonestFibonacciNumbers(10) != 10) {
+        printf("Четвертый тест не прошел");
+        return false;
+    }
+}
+
 void main() {
     setlocale(LC_ALL, "RU");
     if (!tests()) {
         return;
     }
-    printf("Сумма четных чисел Фибоначчи меньших миллиона = %d", sumOfHonestFibonacciNumbers(1000000));
+    if (!testsForBetterFunction()) {
+        return;
+    }
+    printf("Сумма четных чисел Фибоначчи меньших миллиона = %d", betterSumOfHonestFibonacciNumbers(1000000));
 }
