6c660c6d946b9e8e8da6b7ad4dd5b82804e4411f
AmarskiyArtem <amarskiywork@gmail.com>
2022-10-11 23:08:14 +0300

    add reading from file

--- homeworks/C/fourthHomework/modulesAndFiles/modulesAndFiles.c
+++ homeworks/C/fourthHomework/modulesAndFiles/modulesAndFiles.c
@@ -1,46 +1,46 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <locale.h>
 #include "qSort.h"
 #include "theMostCommonElement.h"
 #include <malloc.h>
 
-int arrayLength(int* array) {
-    int length = 0;
-    int i = 0;
-    while (array[i] != NULL) {
-        ++length;
-        ++i;
-    }
-    return length;
-}
-
 int main() {
     setlocale(LC_ALL, "RU");
     FILE *file = fopen("arrayForSorting.txt", "r");
     if (file == NULL) {
-        printf("файл не найден\n");
+        printf("Файл не найден");
         return;
     }
-    char* data[100] = { 0 };
-    int linesRead = 0;
-    while (!feof(file)) {
-        char* buffer = malloc(sizeof(char) * 100);
-        const int readBytes = fscanf(file, "%s", buffer);
-        if (readBytes < 0) {
-            break;
-        }
-        data[linesRead] = buffer;
-        ++linesRead;
+
+    int dataLength = 0;
+    int temp = 0;
+    while (fscanf(file, "%d", &temp) == 1) {
+        ++dataLength;
+    }
+    if (dataLength == 0) {
+        printf("Файл не содержит элементов");
+        return;
+    }
+    fseek(file, 0, SEEK_SET);
+    int* data = (int*)calloc(dataLength, sizeof(int));
+    if (data == NULL) {
+        printf("MEMORY PANICCC");
+        return;
+    }
+
+    for (int i = 0; i < dataLength; ++i) {
+        fscanf_s(file, "%d", &data[i]);
     }
     fclose(file);
-
-    for (int i = 0; i < 15; ++i) {
-        printf("%s\n", data[i]);
+    printf("Массив: \n");
+    for (int i = 0; i < dataLength; ++i) {
+        printf("%d ", data[i]);
     }
-    int dataLength = arrayLength(&data[0]);
-    //printf("%d", dataLength);
-    //quickSort(&data[0], 0, dataLength - 1);
-    //int result = mostCommon(&data[0], dataLength);
-    //printf("Самый частый элемент в файле: %d", result);
+    quickSort(&data[0], 0, dataLength - 1);
+    int result = mostCommon(&data[0], dataLength);
+    printf("\nСамый частый элемент в файле: %d", result);
+    free(data);
 }
