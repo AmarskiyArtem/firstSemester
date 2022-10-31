c9083cf704bb06aeff54132c5da956932a2621b3
AmarskiyArtem <amarskiywork@gmail.com>
2022-10-31 10:56:18 +0300

    add tests

--- homeworks/C/fourthHomework/modulesAndFiles/modulesAndFiles.c
+++ homeworks/C/fourthHomework/modulesAndFiles/modulesAndFiles.c
@@ -7,14 +7,43 @@
 #include "theMostCommonElement.h"
 #include <malloc.h>
 
+bool test(void) {
+    FILE* file = fopen("test.txt", "r");
+    if (file == NULL) {
+        printf("Файл не найден");
+        return false;
+    }
+    int dataLength = 0;
+    int temp = 0;
+    while (fscanf(file, "%d", &temp) == 1) {
+        ++dataLength;
+    }
+    fseek(file, 0, SEEK_SET);
+    int* data = (int*)calloc(dataLength, sizeof(int));
+    if (data == NULL) {
+        printf("MEMORY PANICCC");
+        return false;
+    }
+    for (int i = 0; i < dataLength; ++i) {
+        fscanf_s(file, "%d", &data[i]);
+    }
+    fclose(file);
+    quickSort(&data[0], 0, dataLength - 1);
+    int result = mostCommon(&data[0], dataLength);
+    return result == 65;
+}
+
 int main() {
     setlocale(LC_ALL, "RU");
+    if (!test()) {
+        printf("Тесты упали");
+        return;
+    }
     FILE *file = fopen("arrayForSorting.txt", "r");
     if (file == NULL) {
         printf("Файл не найден");
         return;
     }
-
     int dataLength = 0;
     int temp = 0;
     while (fscanf(file, "%d", &temp) == 1) {
