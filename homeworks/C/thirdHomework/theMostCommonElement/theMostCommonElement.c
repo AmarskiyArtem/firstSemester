a4633ea6b8fb1634a56e011fe77ccf844b2e02d4
AmarskiyArtem <amarskiywork@gmail.com>
2022-11-10 21:36:50 +0300

    add tests

--- homeworks/C/thirdHomework/theMostCommonElement/theMostCommonElement.c
+++ homeworks/C/thirdHomework/theMostCommonElement/theMostCommonElement.c
@@ -28,48 +28,6 @@ void arrayPrint(int* array, int arraySize) {
     }
 }
 
-bool qSortTests(void) {
-    int arrayForTestsLength = 3 + rand() % 40;
-    int* arrayForTests = (int*)calloc(arrayForTestsLength, sizeof(int));
-    if (arrayForTests == NULL) {
-        printf("MEMORY PANICCCC");
-        return false;
-    }
-    int* arrayForTestsCopy = (int*)calloc(arrayForTestsLength, sizeof(int));
-    if (arrayForTestsCopy == NULL) {
-        printf("MEMORY PANICCCC");
-        free(arrayForTests);
-        return false;
-    }
-    for (int i = 0; i < 3; ++i) {
-        for (int i = 0; i < arrayForTestsLength; ++i) {
-            arrayForTests[i] = -100 + rand() % 200;
-            arrayForTestsCopy[i] = arrayForTests[i];
-        }
-        quickSort(&arrayForTests[0], 0, arrayForTestsLength - 1);
-        if (!isSorted(&arrayForTests[0], arrayForTestsLength)) {
-            printf("Sorting error\n Origin array:\n");
-            arrayPrint(&arrayForTestsCopy[0], arrayForTestsLength);
-            printf("\nWrong sorted array:\n");
-            arrayPrint(&arrayForTests[0], arrayForTestsLength);
-            free(arrayForTests);
-            free(arrayForTestsCopy);
-            return false;
-        }
-    }
-    free(arrayForTests);
-    free(arrayForTestsCopy);
-    return true;
-}
-
-bool mostCommonTests(void) {
-    return true;
-}
-
-bool tests(void) {
-    return mostCommonTests() && qSortTests();
-}
-
 int partition(int* array, int low, int high) {
     int pivot = array[high];
     int i = (low - 1);
@@ -109,6 +67,62 @@ int mostCommon(int* array, int arrayLength) {
     }
     return mostCommonElement;
 }
+
+bool qSortTests(void) {
+    int arrayForTestsLength = 3 + rand() % 40;
+    int* arrayForTests = (int*)calloc(arrayForTestsLength, sizeof(int));
+    if (arrayForTests == NULL) {
+        printf("MEMORY PANICCCC");
+        return false;
+    }
+    int* arrayForTestsCopy = (int*)calloc(arrayForTestsLength, sizeof(int));
+    if (arrayForTestsCopy == NULL) {
+        printf("MEMORY PANICCCC");
+        free(arrayForTests);
+        return false;
+    }
+    for (int i = 0; i < 3; ++i) {
+        for (int i = 0; i < arrayForTestsLength; ++i) {
+            arrayForTests[i] = -100 + rand() % 200;
+            arrayForTestsCopy[i] = arrayForTests[i];
+        }
+        quickSort(&arrayForTests[0], 0, arrayForTestsLength - 1);
+        if (!isSorted(&arrayForTests[0], arrayForTestsLength)) {
+            printf("Sorting error\n Origin array:\n");
+            arrayPrint(&arrayForTestsCopy[0], arrayForTestsLength);
+            printf("\nWrong sorted array:\n");
+            arrayPrint(&arrayForTests[0], arrayForTestsLength);
+            free(arrayForTests);
+            free(arrayForTestsCopy);
+            return false;
+        }
+    }
+    free(arrayForTests);
+    free(arrayForTestsCopy);
+    return true;
+}
+
+bool mostCommonTests(void) {
+    int testArray1[] = { 0, 1, 7, 4, 6, 3, 6, 8, 9, 5, 4, 9, 3, 6, 2 }; 
+    int testArray2[] = { 5, 8, 2, 3, 7, 3, 4, 7, 0, 0, 5, 9, 2, 3, 8 };
+    int testArray3[] = { 7, 2, 0, 9, 6, 1, 7, 2, 3, 7, 1, 1, 6, 3, 8 };
+    int testArray[3][15] = { {0, 1, 7, 4, 6, 3, 6, 8, 9, 5, 4, 9, 3, 6, 2},
+                        { 5, 8, 2, 3, 7, 3, 4, 7, 0, 0, 5, 9, 2, 3, 8 },
+                        { 7, 2, 0, 9, 6, 1, 7, 2, 3, 7, 1, 1, 6, 3, 8 } };
+    int results[] = { 6, 3, 7 };
+    for (int i = 0; i < 3; ++i) {
+        quickSort(&testArray[i][0], 0, 14);
+        if ((mostCommon(&testArray[i][0], 15)) != results[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool tests(void) {
+    return mostCommonTests() && qSortTests();
+}
+
 
 void main() {
     srand((unsigned)time(NULL));
