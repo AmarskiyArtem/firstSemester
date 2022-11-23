13c03aacce373094b214d551a568dc9346e9535c
AmarskiyArtem <amarskiywork@gmail.com>
2022-11-23 15:22:28 +0300

    hh

--- homeworks/C/ninthHomework/hashTable/main.c
+++ homeworks/C/ninthHomework/hashTable/main.c
@@ -4,13 +4,12 @@
 
 void main() {
     List* list = createList();
-    isEmpty(list) ? printf("Yes") : printf("No");
     char* a = "fioobnsdf";
     push(list, a);
     char* b = "fhpuranvpon";
     push(list, b);
     push(list, b);
-    char* c = "csdjpvnpb";
+    char *c = "csdjpvnpb";
     push(list, c);
     printList(list);
     deleteList(list);
