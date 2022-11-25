9cffb519d7264e1c715b060941863c817d44105e
AmarskiyArtem <amarskiywork@gmail.com>
2022-11-25 14:43:46 +0300

    after review

--- homeworks/C/fourthHomework/bitwiseOperations/bitwiseOperations.c
+++ homeworks/C/fourthHomework/bitwiseOperations/bitwiseOperations.c
@@ -1,7 +1,8 @@
 ﻿#include <stdio.h>
 #include <locale.h>
 #include <stdbool.h>
-#define sizeOfArrayWithBinaryNumber sizeof(int) * 8
+
+#define SIZE_OF_ARRAY_WITH_BINARY_NUMBER sizeof(int) * 8
 
 void arrayPrint(int* array, int arraySize) {
     for (int i = 0; i < arraySize; ++i) {
@@ -11,16 +12,16 @@ void arrayPrint(int* array, int arraySize) {
 
 void binaryConverter(int* binaryNumber, int number) {
     int bitChecker = 1;
-    for (int i = sizeOfArrayWithBinaryNumber - 1; i >= 0; --i) {
+    for (int i = SIZE_OF_ARRAY_WITH_BINARY_NUMBER - 1; i >= 0; --i) {
         binaryNumber[i] = (number & bitChecker) ? 1 : 0;
-        bitChecker = bitChecker << 1;
+        bitChecker <<= 1;
     }
 }
 
 int decimalConverter(int* binaryNumber) {
     int number = 0;
     int power = 1;
-    for (int i = sizeOfArrayWithBinaryNumber - 1; i >= 0; --i) {
+    for (int i = SIZE_OF_ARRAY_WITH_BINARY_NUMBER - 1; i >= 0; --i) {
         number += binaryNumber[i] * power;
         power *= 2;
     }
@@ -28,25 +29,26 @@ int decimalConverter(int* binaryNumber) {
 }
 void sumOfTwoBinaryNumbers(int* firstNumber, int* secondNumber, int* sumResult) {
     int extraUnit = 0;
-    for (int i = sizeOfArrayWithBinaryNumber - 1; i >= 0; --i) {
+    for (int i = SIZE_OF_ARRAY_WITH_BINARY_NUMBER - 1; i >= 0; --i) {
         int currentSum = firstNumber[i] + secondNumber[i] + extraUnit;
-        sumResult[i] = (currentSum) % 2;
-        extraUnit = (currentSum) / 2;
+        sumResult[i] = currentSum % 2;
+        extraUnit = currentSum / 2;
     }
 }
 
 bool converterTest(void) {
-    int number[sizeOfArrayWithBinaryNumber] = { 0 };
+    int number[SIZE_OF_ARRAY_WITH_BINARY_NUMBER] = { 0 };
     binaryConverter(&number[0], 5);
-    return (number[sizeOfArrayWithBinaryNumber - 1] == 1) && (number[sizeOfArrayWithBinaryNumber - 2] == 0) 
-        && (number[sizeOfArrayWithBinaryNumber - 3] == 1);
+    return (number[SIZE_OF_ARRAY_WITH_BINARY_NUMBER - 1] == 1) && (number[SIZE_OF_ARRAY_WITH_BINARY_NUMBER - 2] == 0) 
+        && (number[SIZE_OF_ARRAY_WITH_BINARY_NUMBER - 3] == 1);
 }
 
 bool decimalTest(void) {
-    int number[sizeOfArrayWithBinaryNumber] = { 0 };
+    int number[SIZE_OF_ARRAY_WITH_BINARY_NUMBER] = { 0 };
     binaryConverter(&number[0], 10);
-    return (decimalConverter(&number[0]) == 10);
+    return decimalConverter(&number[0]) == 10;
 }
+
 bool tests(void) {
     return converterTest() && decimalTest();
 }
@@ -64,19 +66,19 @@ void main() {
     int secondNumber = 0;
     scanf_s("%d", &secondNumber);
 
-    int firstBinaryNumber[sizeOfArrayWithBinaryNumber] = { 0 };
+    int firstBinaryNumber[SIZE_OF_ARRAY_WITH_BINARY_NUMBER] = { 0 };
     binaryConverter(&firstBinaryNumber[0], firstNumber);
-    int secondBinaryNumber[sizeOfArrayWithBinaryNumber] = { 0 };
+    int secondBinaryNumber[SIZE_OF_ARRAY_WITH_BINARY_NUMBER] = { 0 };
     binaryConverter(&secondBinaryNumber[0], secondNumber);
     printf("Первое число в двоичном представлении:\n");
-    arrayPrint(&firstBinaryNumber[0], sizeOfArrayWithBinaryNumber);
+    arrayPrint(&firstBinaryNumber[0], SIZE_OF_ARRAY_WITH_BINARY_NUMBER);
     printf("\nВторое число в двоичном представлении:\n");
-    arrayPrint(&secondBinaryNumber[0], sizeOfArrayWithBinaryNumber);
+    arrayPrint(&secondBinaryNumber[0], SIZE_OF_ARRAY_WITH_BINARY_NUMBER);
 
-    int resultOfSumOfBinaryNumbers[sizeOfArrayWithBinaryNumber] = { 0 };
+    int resultOfSumOfBinaryNumbers[SIZE_OF_ARRAY_WITH_BINARY_NUMBER] = { 0 };
     sumOfTwoBinaryNumbers(&firstBinaryNumber[0], &secondBinaryNumber[0], &resultOfSumOfBinaryNumbers[0]);
     printf("\nИх сумма в двоичном представлении:\n");
-    arrayPrint(&resultOfSumOfBinaryNumbers[0], sizeOfArrayWithBinaryNumber);
+    arrayPrint(&resultOfSumOfBinaryNumbers[0], SIZE_OF_ARRAY_WITH_BINARY_NUMBER);
     int decimalResult = decimalConverter(&resultOfSumOfBinaryNumbers[0]);
     printf("\nВ десятичном:\n%d", decimalResult);
 }
