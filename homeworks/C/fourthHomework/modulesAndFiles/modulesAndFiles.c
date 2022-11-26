123641a2160bc109462b57723ede543c8f8fe59b
AmarskiyArtem <amarskiywork@gmail.com>
2022-11-26 12:20:58 +0300

    move opening file to separate func and add tests for right opening

--- homeworks/C/fourthHomework/modulesAndFiles/modulesAndFiles.c
+++ homeworks/C/fourthHomework/modulesAndFiles/modulesAndFiles.c
@@ -9,63 +9,78 @@
 #include "qSort.h"
 #include "theMostCommonElement.h"
 
-bool test(void) {
-    FILE* file = fopen("test.txt", "r");
+
+int* readingFromFile(char* fileName, int* dataLength) {
+    FILE* file = fopen(fileName, "r");
     if (file == NULL) {
         printf("Файл не найден");
-        return false;
+        return NULL;
     }
-    int dataLength = 0;
+    *dataLength = 0;
     int temp = 0;
     while (fscanf(file, "%d", &temp) == 1) {
-        ++dataLength;
+        ++(*dataLength);
+    }
+    if (dataLength == 0) {
+        printf("Файл не содержит элементов");
+        fclose(file);
+        return NULL;
     }
     fseek(file, 0, SEEK_SET);
-    int* data = (int*)calloc(dataLength, sizeof(int));
+    int* data = (int*)calloc(*dataLength, sizeof(int));
     if (data == NULL) {
         printf("MEMORY PANICCC");
-        return false;
+        fclose(file);
+        return NULL;
     }
-    for (int i = 0; i < dataLength; ++i) {
+
+    for (int i = 0; i < *dataLength; ++i) {
         fscanf_s(file, "%d", &data[i]);
     }
     fclose(file);
-    quickSort(&data[0], 0, dataLength - 1);
-    int result = mostCommon(&data[0], dataLength);
-    return result == 65;
+    return data;
+}
+
+bool readingFromFileTest(void) {
+    int testDataLegth = 0;
+    int* testData = readingFromFile("test.txt", &testDataLegth);
+    int rightArray[] = { 65, 22, 41, 67, 1 };
+    for (int i = 0; i < testDataLegth; ++i) {
+        if (testData[i] != rightArray[i]) {
+            free(testData);
+            return false;
+        }
+    }
+    free(testData);
+    return true;
+}
+
+bool mostCommonTest(void) {
+    int testArray[3][15] = { {0, 1, 7, 4, 6, 3, 6, 8, 9, 5, 4, 9, 3, 6, 2},
+                    { 5, 8, 2, 3, 7, 3, 4, 7, 0, 0, 5, 9, 2, 3, 8 },
+                    { 7, 2, 0, 9, 6, 1, 7, 2, 3, 7, 1, 1, 6, 3, 8 } };
+    int results[] = { 6, 3, 7 };
+    for (int i = 0; i < 3; ++i) {
+        quickSort(&testArray[i][0], 0, 14);
+        if ((mostCommon(&testArray[i][0], 15)) != results[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool tests(void) {
+    return mostCommonTest() && readingFromFileTest();
 }
 
 int main() {
     setlocale(LC_ALL, "RU");
-    if (!test()) {
+    if (!tests()) {
         printf("Тесты упали");
         return;
     }
-    FILE *file = fopen("arrayForSorting.txt", "r");
-    if (file == NULL) {
-        printf("Файл не найден");
-        return;
-    }
     int dataLength = 0;
-    int temp = 0;
-    while (fscanf(file, "%d", &temp) == 1) {
-        ++dataLength;
-    }
-    if (dataLength == 0) {
-        printf("Файл не содержит элементов");
-        return;
-    }
-    fseek(file, 0, SEEK_SET);
-    int* data = (int*)calloc(dataLength, sizeof(int));
-    if (data == NULL) {
-        printf("MEMORY PANICCC");
-        return;
-    }
-
-    for (int i = 0; i < dataLength; ++i) {
-        fscanf_s(file, "%d", &data[i]);
-    }
-    fclose(file);
+    int* data = readingFromFile("arrayForSorting.txt", &dataLength);
     printf("Массив: \n");
     for (int i = 0; i < dataLength; ++i) {
         printf("%d ", data[i]);
