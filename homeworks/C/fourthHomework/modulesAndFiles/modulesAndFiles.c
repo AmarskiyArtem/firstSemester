6c1aab609f2917c6da87cf9a5a5131704d420243
AmarskiyArtem <amarskiywork@gmail.com>
2022-12-02 14:35:15 +0300

    after review

--- homeworks/C/fourthHomework/modulesAndFiles/modulesAndFiles.c
+++ homeworks/C/fourthHomework/modulesAndFiles/modulesAndFiles.c
@@ -9,11 +9,9 @@
 #include "qSort.h"
 #include "theMostCommonElement.h"
 
-
-int* readingFromFile(char* fileName, int* dataLength) {
+int* readingFromFile(const char* fileName, int* dataLength) {
     FILE* file = fopen(fileName, "r");
     if (file == NULL) {
-        printf("Файл не найден");
         return NULL;
     }
     *dataLength = 0;
@@ -22,14 +20,12 @@ int* readingFromFile(char* fileName, int* dataLength) {
         ++(*dataLength);
     }
     if (dataLength == 0) {
-        printf("Файл не содержит элементов");
         fclose(file);
         return NULL;
     }
     fseek(file, 0, SEEK_SET);
     int* data = (int*)calloc(*dataLength, sizeof(int));
     if (data == NULL) {
-        printf("MEMORY PANICCC");
         fclose(file);
         return NULL;
     }
@@ -45,12 +41,17 @@ bool readingFromFileTest(void) {
     int testDataLegth = 0;
     int* testData = readingFromFile("test.txt", &testDataLegth);
     int rightArray[] = { 65, 22, 41, 67, 1 };
+    if (testDataLegth != 5) {
+        return false;
+    }
+
     for (int i = 0; i < testDataLegth; ++i) {
         if (testData[i] != rightArray[i]) {
             free(testData);
             return false;
         }
     }
+
     free(testData);
     return true;
 }
@@ -73,7 +74,7 @@ bool tests(void) {
     return mostCommonTest() && readingFromFileTest();
 }
 
-int main() {
+void main() {
     setlocale(LC_ALL, "RU");
     if (!tests()) {
         printf("Тесты упали");
@@ -81,6 +82,9 @@ int main() {
     }
     int dataLength = 0;
     int* data = readingFromFile("arrayForSorting.txt", &dataLength);
+    if (data == NULL) {
+        return;
+    }
     printf("Массив: \n");
     for (int i = 0; i < dataLength; ++i) {
         printf("%d ", data[i]);
