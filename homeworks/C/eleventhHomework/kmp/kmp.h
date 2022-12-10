46e45b4aea1cc41e124e31977427c951954bc39f
AmarskiyArtem <amarskiywork@gmail.com>
2022-12-10 16:44:35 +0300

    complete task

--- homeworks/C/eleventhHomework/kmp/kmp.h
+++ homeworks/C/eleventhHomework/kmp/kmp.h
@@ -1,3 +1,4 @@
 #pragma once
 
+//returns index of the first occurrence of the substring, -1 if there are no occurrences
 int searchForOccurrences(char* string, char* substring);
--- homeworks/C/eleventhHomework/kmp/main.c
+++ homeworks/C/eleventhHomework/kmp/main.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "kmp.h"
+
+#define MAX_SUBSTRING_LENGTH 100
 
 char* readFromFile(const char* filename) {
     int stringLength = 0;
@@ -33,6 +36,48 @@ char* readFromFile(const char* filename) {
     return string;
 }
 
+bool readFromFileTest(void) {
+    int testStringLength = 0;
+    char* testString = readFromFile("test.txt");
+    if (strcmp(testString, "nvfdsnvs\nnewrg") != 0) {
+        free(testString);
+        return false;
+    }
+    free(testString);
+    return true;
+}
+
+bool kmpTest(void) {
+    return searchForOccurrences("fhsduavp", "sd") == 2 &&
+        searchForOccurrences("nebhvbovb", "aaa") == -1 &&
+        searchForOccurrences("abababbaba", "abba") == 4;
+}
+
+bool tests(void) {
+    return readFromFileTest() && kmpTest();
+}
+
 void main() {
-    
+    if (!tests()) {
+        printf("tests failed");
+        return;
+    }
+    char* string = readFromFile("input.txt");
+    if (string == NULL) {
+        printf("File missing");
+        return;
+    }
+    printf("Text from file:\n%s", string);
+    printf("\nInput the substring:\n");
+    char* substring = calloc(MAX_SUBSTRING_LENGTH, sizeof(char));
+    if (substring == NULL) {
+        printf("Memory allocation error");
+        return;
+    }
+    scanf_s("%100s", substring, (unsigned)sizeof(substring));
+    const int result = searchForOccurrences(string, substring);
+    result >= 0 ?
+        printf("The first occurrence of the symbol begins with %d index", result) :
+        printf("The substring does not occur in the file");
+    free(substring);
 }
