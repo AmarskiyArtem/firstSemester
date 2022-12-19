67620d4d089aafcf17a8b355f308734e3870eb99
AmarskiyArtem <amarskiywork@gmail.com>
2022-12-19 19:03:15 +0300

    dd

--- homeworks/C/seventhHomework/ParsingTree/main.c
+++ homeworks/C/seventhHomework/ParsingTree/main.c
@@ -1,1 +1,33 @@
-#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "tree.h"
+#include <string.h>
+
+#define MAX_STRING_LENGTH 50
+
+char* getStringFromFile(const char* fileName) {
+    FILE* file = fopen(fileName, "r");
+    if (file == NULL) {
+        return NULL;
+    }
+    char* string = calloc(MAX_STRING_LENGTH, sizeof(char));
+    if (string == NULL) {
+        fclose(file);
+        return NULL;
+    }
+    fgets(string, MAX_STRING_LENGTH, file);
+    return string;
+}
+
+bool isCorrectMathExpression(const char* string) {
+    const char correctSymbols[] = "0123456789/*-+() ";
+    int stringLength = strlen(string) - 1;
+    for (int i = 0; i < stringLength; ++i) {
+        if (strchr(correctSymbols, string[i]) == NULL) {
+            return false;
+        }
+    }
+    return true;
+}
+
--- homeworks/C/seventhHomework/ParsingTree/tree.c
+++ homeworks/C/seventhHomework/ParsingTree/tree.c
@@ -1,1 +1,22 @@
-﻿#pragma once
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    char operation;
+    int operand;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+typedef struct Tree {
+    Node* root;
+} Tree;
+
+bool isEmpty(Tree* tree) {
+    return tree->root == NULL;
+}
+
