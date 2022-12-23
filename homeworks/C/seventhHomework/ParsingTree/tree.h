e543c13ff2c4a5d0fbad6a6850e3c909d396cc9e
AmarskiyArtem <amarskiywork@gmail.com>
2022-12-23 03:49:12 +0300

    add comments

--- homeworks/C/seventhHomework/ParsingTree/tree.h
+++ homeworks/C/seventhHomework/ParsingTree/tree.h
@@ -12,12 +12,17 @@ typedef enum ErrorCode {
     zeroDivision
 } ErrorCode;
 
+// Creates the tree
 Tree* createTree(void);
 
+// Fill tree by given string
 ErrorCode fillTree(Tree* tree, const char* string);
 
+// Calculate the math expression in tree
 int calculate(Tree* tree, ErrorCode* errorCode);
 
+// Delete tree
 void deleteTree(Tree* tree);
 
+// Print tree
 ErrorCode printTree(Tree* tree);
